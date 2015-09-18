8b508a410128b30d2eae2287cf0feb1958561cb0
unnu3590 <unnu3590@ISS147D8-10.scdsb.pri>
2015-09-18 10:39:23 -0400

    initial

--- src/employeePay.java
+++ src/employeePay.java
@@ -3,7 +3,20 @@ public class employeePay {
 
     
     public static void main(String[] args) {
+         Console c = new Console();
          
+         double hours, rate=15.00,pay;
+         int employee[]=new int[5];
+         
+         for(int x=0;x<=5;x++){
+          
+             while(true){
+              c.print("Enter the number of hours for employee "+x);   
+             }
+         }
+             
+        
+         } 
     }
     
 }
