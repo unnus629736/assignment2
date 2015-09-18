55e659187f96dfb9c0185502d4ecf5ea3cb18123
unnu3590 <unnu3590@ISS171D8-20.scdsb.pri>
2015-09-18 13:01:27 -0400

    almost done *+

--- src/employeePay.java
+++ src/employeePay.java
@@ -5,18 +5,38 @@ public class employeePay {
     public static void main(String[] args) {
          Console c = new Console();
          
-         double hours, rate=15.00,pay;
-         int employee[]=new int[5];
+         double hours=0, rate=15.00,regpay,totalpay,overpay=0, grandtotal, totalreg, totalover;
+         String employee[]=new String[5];
          
-         for(int x=0;x<=5;x++){
+         for(int x=1;x<=5;x++){
           
-             while(true){
-              c.print("Enter the number of hours for employee "+x);   
+             do{
+                 c.print("Enter the number of hours for employee "+x+" --> ");
+                 hours=c.readDouble();
+                 
+             }while(hours<0||hours>70);
+             if(hours<40){
+                 regpay=rate*hours;   
              }
+             else {
+              regpay=rate*40;
+              overpay=rate*(hours-40);
+             }
+             c.println("Regular pay= $"+regpay);
+             c.println("Overtime pay= $"+overpay);
+             totalpay=regpay+overpay;
+             c.println("Total Pay= $"+totalpay);
+             grandtotal=+totalpay;
+             totalreg=+regpay;
+             totalover=+overpay;
+             }
+         c.println("");
+         c.println("");
+         c.println("");
          }
              
         
          } 
-    }
     
-}
+    
+
